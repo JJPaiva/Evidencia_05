5cd7cb7a7c94d8aa887388b381355d45b26a333f
Juan Paiva <128234958+JJPaiva@users.noreply.github.com>
2023-04-24 20:15:58 -0400

    Add files via upload

---
+++ Pelota_Basketball.java
@@ -0,0 +1,40 @@
+
+
+public class Pelota_Basketball {
+
+    String marca;
+    String material;
+    int tamaño;
+    String dueño;
+    String estado; //Dañada, nueva, usada, pinchada, etc...
+
+
+    public void Agarrar(){
+
+    }
+
+    public void Soltar(){
+
+    }
+
+    public void Botar(){
+
+    }
+
+    public void Lanzar(){
+
+    }
+
+    public void Inflar(){
+
+    }
+
+    public void Desinflar(){
+
+    }
+
+    public void Limpiar(){
+
+    }
+
+}
